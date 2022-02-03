3cb1a1dd93d25f7d062841c3ff14d4706e2e6ce0
WormerKing <aferinsanaaferin@gmail.com>
2022-02-03 16:13:08 +0300

    Seed dosyası eklendi

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
-Topic.all.each do |topic|
-	Topic.reset_counters topic.id,:comments
+arr = ["Python ve ailesi","Ruby ve ailesi","Proglamlama temelleri","Basit HTML ve CSS","Genel Konular","Javascript","SCSS"]
+
+arr.each do |i|
+	puts "-- #{i} forumu oluşturuldu" if Forum.find_or_create_by(name:i) 
 end
